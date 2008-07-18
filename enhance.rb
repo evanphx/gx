eff69b62786988c9d3b96349e993fe8fe2434467
Evan Phoenix <ephoenix@engineyard.com>
2008-07-17 22:17:43 -0600

    Initial version

---
+++ enhance.rb
@@ -0,0 +1,79 @@
+require 'rubygems'
+require 'grit'
+
+module Grit
+  class Repo
+    # Given +hashish+, parse it and return the hash
+    # it refers to.
+    #
+    def resolve_rev(hashish)
+      hash = @git.rev_parse({:verify => true}, hashish)
+      return nil if $?.exitstatus != 0
+      return hash.strip
+    end
+
+    # Given +left+ and +right+, detect and return their
+    # closest common ancestor. Used to find the point to perform
+    # merges from.
+    #
+    # +right+ defaults to the current HEAD.
+    #
+    def find_ancestor(left, right=nil)
+      right ||= resolve_rev "HEAD"
+      hash = @git.merge_base({}, left, right)
+      return nil if $?.exitstatus != 0
+      return hash.strip
+    end
+
+    def revs_between(left, right)
+      @git.rev_list({}, "#{left}..#{right}").split("\n")
+    end
+
+    class UnmergedFile
+      def initialize(name)
+        @name = name
+      end
+
+      attr_accessor :original, :mine, :yours
+    end
+
+    def unmerged_files
+      files = Hash.new { |h,k| h[k] = UnmergedFile.new(k) }
+      @git.ls_files({:u => true}).split("\n").each do |line|
+        mode, hash, stage, name = line.split(/\s+/, 4)
+        case stage
+        when "1"
+          files[name].original = hash
+        when "2"
+          files[name].yours = hash
+        when "3"
+          files[name].mine = hash
+        end
+      end
+
+      return files
+    end
+
+    def am_info
+      info = {}
+      File.open(".dotest/info") do |f|
+        f.readlines.each do |line|
+          line.strip!
+          break if line.empty?
+          key, val = line.split(": ")
+          info[key.downcase.to_sym] = val
+        end
+      end
+
+      if subject = info[:subject]
+        subject.gsub!(/^\[PATCH\] /,"")
+      end
+
+      return info
+    end
+
+    def to_be_committed
+      @git.diff_index({:cached => true, :name_only => true}, "HEAD").split("\n")
+    end
+  end
+end
---
+++ git-update.rb
@@ -0,0 +1,299 @@
+#!/usr/bin/env ruby
+
+# TODO:
+# * Handle a conflict in a remote commit that is in a renamed file.
+
+$:.unshift File.dirname(__FILE__)
+
+require 'enhance'
+require 'optparse'
+require 'ostruct'
+require 'fileutils'
+require 'readline'
+
+opts = OpenStruct.new
+
+op = OptionParser.new do |o|
+  o.on "-z", "--analyze", "Output information on what update would do" do
+    opts.analyze = true
+  end
+
+  o.on "-v", "--verbose", "Be verbose" do
+    opts.verbose = true
+  end
+
+  o.on "--debug", "Show all git commands run" do
+    Grit.debug = true
+  end
+
+  o.on "-q", "--quiet", "Show the minimal output" do
+    opts.quiet = true
+  end
+end
+
+op.parse!(ARGV)
+
+STDOUT.sync = true
+
+top = `git rev-parse --show-cdup 2>&1`.strip
+
+Dir.chdir top unless top.empty?
+
+repo = Grit::Repo.new(".")
+
+print "Fetching new commits: "
+out = repo.git.fetch({}, "2>&1")
+puts "done."
+
+# TODO parse +out+ for details to show the user.
+
+current = repo.resolve_rev "HEAD"
+origin = repo.resolve_rev "origin"
+
+branch = repo.git.symbolic_ref({:q => true}, "HEAD").strip
+
+common = repo.find_ancestor(origin, current)
+  
+to_replay = repo.revs_between(common, current)
+to_receive = repo.revs_between(common, origin)
+
+if opts.analyze
+  puts "Closest ancestor between HEAD and origin: #{common[0,7]}"
+  puts
+
+  if to_receive.empty?
+    puts "Current history is up to date."
+    exit 0
+  end
+
+  puts "#{to_receive.size} new commits."
+  if opts.verbose
+    system "git log --pretty=oneline #{common}..origin"
+    puts
+  end
+
+  puts "#{to_replay.size} commits to adapt."
+  if opts.verbose
+    system "git log --pretty=oneline #{common}..HEAD"
+    puts
+  end
+
+  exit 0
+end
+
+HELP = <<-TXT
+You're currently inside the conflict resolver. The following commands
+are available to help you.
+
+When the conflict resolver is first started, the contents of the file
+will contain the file populated with conflict markers for you to edit.
+
+[D]iff       View the diffs between the (original version and local version) 
+                and (original version and remote version).
+[E]dit       Launch your editor to edit the file.
+[T]ool       Run git-mergetool on the file.
+[O]riginal   Set the contents of the file to the original version. This is
+                version from the common ancestor of your commit and the remote
+                commit.
+[M]ine       Set the contents of the file to be your version.
+[R]emote     Set the contents of the file to be the remote version.
+co[N]flict   Set the contents of the file to contain the merged between the
+                local version and remote version, with conflict markers.
+[P]rompt     Launch a subshell to deal with the conflict. Simply exit
+                from the shell to continue with conflict resolution.
+[I]nfo       View information about the commit and the current file.
+[A]bort      Cancel the update all together, restore everything to before
+                the update was started.
+[C]ontinue   You're done dealing with this conflict, move on to the next one.
+[H]elp       Detail all available options, you're looking at it now.
+TXT
+
+def includes_conflict_markers?(path)
+  /^<<<<<<< HEAD/.match(File.read(path))
+end
+
+def handle_unmerged(patch_info, repo, files)
+  files.each do |name, info|
+    system "cp #{name} .git/with_markers"
+    
+    puts
+    puts "Conflict discovered in '#{name}'"
+    
+    loop do
+
+      # If there are conflict markers, default is edit.
+      if includes_conflict_markers?(name)
+        default = "E"
+
+      # otherwise it's continue.
+      else
+        default = "C"
+      end
+
+      ans = Readline.readline "Select: [D]iff, [E]dit, [C]ontinue, [H]elp: [#{default}] "
+      ans = default if ans.empty?
+      want = ans.downcase[0]
+      case want
+      when ?d
+        orig = ".git/diff/original/#{name}"
+        FileUtils.mkdir_p File.dirname(orig)
+        repo.git.cat_file({}, "blob", info.original, " > #{orig}")
+        
+        mine = ".git/diff/mine/#{name}"
+        FileUtils.mkdir_p File.dirname(mine)
+        repo.git.cat_file({}, "blob", info.mine, " > #{mine}")
+        
+        remote = ".git/diff/remote/#{name}"
+        FileUtils.mkdir_p File.dirname(remote)
+        repo.git.cat_file({}, "blob", info.yours, " > #{remote}")
+
+        system "cd .git/diff; diff -u original/#{name} mine/#{name}"
+        system "cd .git/diff; diff -u original/#{name} remote/#{name}"
+        system "rm -rf .git/diff"
+      when ?e
+        system "#{ENV['EDITOR']} #{name}"
+      when ?t
+        system "git mergetool #{name}"
+      when ?o
+        repo.git.cat_file({}, "blob", info.original, " > #{name}")
+      when ?m
+        repo.git.cat_file({}, "blob", info.mine, " > #{name}")
+      when ?r
+        repo.git.cat_file({}, "blob", info.yours, " > #{name}")
+      when ?n
+        system "cp .git/with_markers #{name}"
+      when ?p
+        puts "Starting a sub-shell to handle conflicts for #{name}."
+        puts "Exit the shell to continue resolution."
+        system "$SHELL"
+      when ?i
+        puts "Current file: #{name}"
+        puts "Current commit:"
+        puts "  Subject: #{patch_info[:subject]}"
+        puts "  Date:    #{patch_info[:date]}"
+        puts "  Author:  #{patch_info[:author]} (#{patch_info[:email]})"
+      when ?a
+        raise "abort!"
+      when ?h
+        puts HELP
+      when ?c
+        if includes_conflict_markers?(name)
+          puts
+          puts "It looks like this file still contains conflict markers."
+          a = Readline.readline "Are you sure that you want to commit it? [Y/N]: "
+          break if a.downcase[0] == ?y
+        else
+          break
+        end
+      else
+        puts "Unknown option. Try again."
+      end
+    end
+
+    File.unlink ".git/with_markers" rescue nil
+    repo.git.add({}, name)
+  end
+end
+
+if to_receive.empty?
+  puts "Up to date."
+  exit 0
+end
+
+if opts.verbose
+  puts "Extracting commits between #{common[0,7]} and HEAD..."
+end
+
+# Switch back in time so we can re-apply commits. checkout
+# will return non-zero if there it can't be done. In that case
+# we perform a WIP commit, and unwind that WIP commit later,
+# leaving the working copy the same way it was.
+
+used_wip = false
+
+repo.git.checkout({:q => true}, origin, "2>&1")
+if $?.exitstatus != 0
+  repo.git.commit({:m => "'++WIP++'", :a => true})
+  used_wip = true
+
+  # Because we've introduced a new commit, we need to repoint current.
+  current = repo.resolve_rev "HEAD"
+ 
+  # And the list of commits to replay.
+  to_replay = repo.revs_between(common, current)
+
+  # Ok, try again.
+  error = repo.git.checkout({:q => true}, origin, "2>&1")
+  if $?.exitstatus != 0
+    # Ok, give up.
+    # First get rid of the wip.
+    repo.git.reset({:mixed => true}, "HEAD^")
+
+    # Now tell the user what happened.
+    puts "ERROR: Sorry, 'git checkout' can't figure out how to properly switch"
+    puts "the working copy. Please fix this and run 'git update' again."
+    puts "Here is the error that 'git checkout' reported:"
+    puts error
+    exit 1
+  end
+end
+
+system "git format-patch --stdout #{common}..#{current} > .git/update-patch"
+out = repo.git.am({:rebasing => true}, " < .git/update-patch 2> /dev/null")
+while $?.exitstatus != 0
+  info = repo.am_info
+  if opts.verbose
+    if info[:subject] == "++WIP++"
+      puts "Conflict detected in working copy."
+    else
+      puts "Conflict detected applying: #{info[:subject]}"
+    end
+  end
+
+  unmerged = repo.unmerged_files
+  begin
+    handle_unmerged info, repo, unmerged
+  rescue RuntimeError
+    puts "Aborting update..."
+    repo.git.reset({:hard => true}, current)
+    repo.git.checkout({}, branch.gsub(%r!^refs/heads/!, ""))
+    
+    if used_wip
+      repo.git.reset({:mixed => true}, "HEAD^")
+    end
+
+    system "rm -rf .dotest"
+
+    exit 1
+  end
+
+  if repo.to_be_committed.empty?
+    out = repo.git.am({:skip => true, "3" => true})
+  else
+    out = repo.git.am({:resolved => true, "3" => true})
+  end
+end
+
+# Remove the patch we created contain all the rebased commits
+File.unlink ".git/update-patch" rescue nil
+
+rev = repo.resolve_rev "HEAD"
+
+# Update the branch ref to point to our new commit
+
+repo.git.update_ref({:m => "updated"}, branch, rev, current)
+repo.git.symbolic_ref({}, "HEAD", branch)
+
+# If we inserted a WIP commit on the top, remove the commit, but leave
+# the work.
+if used_wip
+  repo.git.reset({:mixed => true}, "HEAD^")
+end
+
+puts
+puts "Updated. Imported #{to_receive.size} commits, HEAD now pointed to #{rev[0,7]}."
+puts
+
+unless opts.quiet
+  system "git diff --stat #{common}..#{origin}"
+end
