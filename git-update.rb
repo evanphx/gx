4050cfc59803f12ea42cb79f9a73cb1e5f3a687d
Evan Phoenix <ephoenix@engineyard.com>
2008-07-29 16:03:24 -0700

    warn when a branch doesnt have merge info

--- git-update.rb
+++ git-update.rb
@@ -49,6 +49,13 @@ branch = repo.git.symbolic_ref({:q => true}, "HEAD").strip
 branch_name = branch.gsub %r!^refs/heads/!, ""
 
 origin_ref = repo.merge_ref branch_name
+unless origin_ref
+  puts "Sorry, it appears you're current branch is not set setup with merge info."
+  puts "Please set 'branch.#{branch_name}.remote' and 'branch.#{branch_name}.merge'"
+  puts "and try again."
+  exit 1
+end
+
 origin = repo.resolve_rev origin_ref
 
 common = repo.find_ancestor(origin, current)
